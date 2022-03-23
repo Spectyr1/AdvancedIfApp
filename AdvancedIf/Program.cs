7456b3f3f6baad89162306f99df7280aa50b50a4
Danny Warren <danny@theforbiddenarts.com>
2022-03-23 15:27:06 -0400

    code submition

--- AdvancedIf/Program.cs
+++ AdvancedIf/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.Write("What is your first name: ");
+string? firstName = Console.ReadLine();
+
+Console.Write("What is your last name: ");
+string? lastName = Console.ReadLine();
+
+if (firstName.ToLower() == "tim" && lastName.ToLower() == "corey")
+{
+    Console.WriteLine("Hello professor Corey.");
+}
+else if (firstName.ToLower() == "tim" || lastName.ToLower() == "corey")
+{
+    Console.WriteLine("You have a great part in your name.");
+}
+else
+{
+    Console.WriteLine("Hello student.");
+}
+
+//if (firstName.ToLower() == "tim" &&
+//    lastName.ToLower() == "corey")
+//{
+//    Console.WriteLine("Hello Mr. Corey.");
+//}
+//else if (firstName.ToLower() == "tim")
+//{
+//    Console.WriteLine("Hello, you have a cool first name.");
+//}
+//else if (lastName.ToLower() == "corey")
+//{
+//    Console.WriteLine("You have a great last name.");
+//}
+//else
+//{
+//    Console.WriteLine("Sorry you don't have a cooler name.");
+//}
+
+
+//if (firstName.ToLower() == "tim")
+//{
+//    Console.WriteLine("You have a cool first name");
+//}
+
+//if (lastName.ToLower() == "corey")
+//{
+//    Console.WriteLine("You have a great last name.");
+//}
+//else
+//{
+//    Console.WriteLine("Sorry your name isn't cooler.");
+//}
+
+int age = 43;
+
+// ==, >, >=, <, <=, !=
+//if (age != 43)
+//{
+//    Console.WriteLine("Sorry, you aren't a great age.");
+//}
+
+if ((age >= 40 && age < 50) || (age >= 70 && age < 80))
+{
+    Console.WriteLine("You are in your 40's");
+}
